56a028e5f0c2c08e5d28e3517d80d3bbe504f950
Johnny <johnathan.mnn@gmail.com>
2023-03-13 15:46:47 -0500

    Envelop animation working

--- client/src/pages/Note.js
+++ client/src/pages/Note.js
@@ -1,7 +1,18 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import $ from "jquery";
-import "../styles/note.css"
+import "../styles/note.css";
+import "../styles/envelope.css";
+
+import redStar from "../assets/imgs/red-star.png";
+import orangeStar from "../assets/imgs/orange-star.png";
+import yellowStar from "../assets/imgs/yellow-star.png";
+import greenStar from "../assets/imgs/green-star.png";
+import blueStar from "../assets/imgs/blue-star.png";
+import purpleStar from "../assets/imgs/purple-star.png";
+import pinkStar from "../assets/imgs/pink-star.png";
+import blackStar from "../assets/imgs/black-star.png";
+let star;
 
 export default function Note(){
     let { uniqId } = useParams();
@@ -18,14 +29,20 @@ export default function Note(){
       getNote();
     });
 
+    if(note.sticker === "red"){
+      star = redStar
+  }
+
     return(
-        <div className="">
+        <div className={note.sticker}>
             <div className="envelope-container">
             <input id="flap" type="checkbox"/>
             <label className="flap" for="flap"></label>
             <div className="envelope-back"></div>
             <div className="card">
-            Dear {note.recipient},
+            <h3 className="note-h3">Dear {note.recipient},</h3>
+            <p>{note.body}</p>
+            <h3 className="note-h3">Sincerely, <br /> {note.sender}</h3>
             </div>
         </div>
     </div>
